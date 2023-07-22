dbe77b7ff4098c6b3a310c506420820f69d484a5
IamRodion <22568039+IamRodion@users.noreply.github.com>
2023-07-21 21:29:59 -0500

    Diseño web de prueba para MirageTrades Página de prueba para definir estilos y UI de la futura AppWeb MirageTrades

---
+++ js/script.js
@@ -0,0 +1,200 @@
+// const main = $("#main")
+
+// $.ajax({
+//     url: "https://run.mocky.io/v3/3df76b76-cfc9-44c6-a62c-0bd039b0889f",
+//     method: "GET"
+// }).then(results => {
+//     for (let i = 0; i < results.length; i++) {
+//         let tr = "<tr><td>" + results[i].icon + "</td><td>" + results[i].name + "</td><td>" + results[i].primary + " " + results[i].primary_type + "</td><td>" + results[i].secondary + " " + results[i].secondary_type + "</td><td>"+ results[i].bonus_1 + ", " + results[i].bonus_2 + "</td><td>" + results[i].enchant + "</td><td>" + results[i].elite + "</td><td>" + results[i].price + ' <span><img src="/img/icons/others/Gold.png" alt="Gold Logo" style="height: 20px;"></span>' + "</td><td>" + results[i].seller + "</td></tr>"
+//         // let tr = "<tr><td>" + results[i].id + "</td><td>" + results[i].enchant + ' <span><img src="/img/icons/others/Element-fire.png" alt="Gold Logo" style="height: 20px;"></span>' + "</td><td>" + results[i].elite + ' <span><img src="/img/icons/others/Elite.webp" alt="Elite Logo" style="height: 20px;"></span>' + "</td><td>" + results[i].price + ' <span><img src="/img/icons/others/Gold.png" alt="Gold Logo" style="height: 20px;"></span>' + "</td><td>" + results[i].seller + "</td></tr>"
+//         main.append(tr)
+//     }
+// }
+// )
+
+// icon name primary primary_type secondary secondary_type bonus_1 bonus_2 enchant elite price seller
+// Indigo Sandals, Arnisium Boots, Crystal Boots, Spiked Boots, Sentinel Boots, Mythril Boots, Emerald Boots, Granite Boots, Golden Boots
+
+
+// [
+//     {
+//       "icon": "@",
+//       "name": "Royal Boots",
+//       "primary": 20,
+//       "primary_type": "Magic",
+//       "secondary": 10,
+//       "secondary_type": "Magic",
+//       "bonus_1": "Omni Resist",
+//       "bonus_2": "Crit Chance",
+//       "enchant": "Fire",
+//       "elite": True,
+//       "price": 200,
+//       "seller": "Juan"
+//     },
+//     {
+//       "id": 2,
+//       "enchant": "Fire",
+//       "elite": false,
+//       "price": 500,
+//       "seller": "Pedro"
+//     },
+//     {
+//       "id": 3,
+//       "enchant": "Fire",
+//       "elite": true,
+//       "price": 2000,
+//       "seller": "Maria"
+//     },
+//     {
+//       "id": 4,
+//       "enchant": "Fire",
+//       "elite": true,
+//       "price": 1500,
+//       "seller": "Luis"
+//     },
+//     {
+//       "id": 5,
+//       "enchant": "Fire",
+//       "elite": false,
+//       "price": 300,
+//       "seller": "Ana"
+//     },
+//     {
+//       "id": 6,
+//       "enchant": "Fire",
+//       "elite": true,
+//       "price": 2500,
+//       "seller": "Carlos"
+//     }
+//   ]
+
+
+
+[
+    {
+        "icon": "@",
+        "name": "Indigo Sandals",
+        "primary": 22,
+        "primary_type": "Magic",
+        "secondary": 11,
+        "secondary_type": "Magic",
+        "bonus_1": "Crit Chance",
+        "bonus_2": "HP Regen",
+        "enchant": "Fire",
+        "elite": true,
+        "price": 200,
+        "seller": "Rodion"
+    },
+    {
+        "icon": "@",
+        "name": "Arnisium Boots",
+        "primary": 18,
+        "primary_type": "Magic",
+        "secondary": 6,
+        "secondary_type": "Magic",
+        "bonus_1": "Mana Regen",
+        "bonus_2": "Leech",
+        "enchant": "Ice",
+        "elite": false,
+        "price": 250,
+        "seller": "Hippop"
+    },
+    {
+        "icon": "@",
+        "name": "Crystal Boots",
+        "primary": 20,
+        "primary_type": "Magic",
+        "secondary": 10,
+        "secondary_type": "Magic",
+        "bonus_1": "Crit Chance",
+        "bonus_2": "Mana Regen",
+        "enchant": "Energy",
+        "elite": false,
+        "price": 20,
+        "seller": "Carlos we"
+    },
+    {
+        "icon": "@",
+        "name": "Spiked Boots",
+        "primary": 19,
+        "primary_type": "Distance",
+        "secondary": 8,
+        "secondary_type": "Distance",
+        "bonus_1": "Money Find",
+        "bonus_2": "Thorns",
+        "enchant": "Nature",
+        "elite": true,
+        "price": 100,
+        "seller": "Chezka May"
+    },
+    {
+        "icon": "@",
+        "name": "Sentinel Boots",
+        "primary": 18,
+        "primary_type": "Distance",
+        "secondary": 9,
+        "secondary_type": "Distance",
+        "bonus_1": "Potion Bonus",
+        "bonus_2": "Money Find",
+        "enchant": "Physical",
+        "elite": true,
+        "price": 50,
+        "seller": "Krey"
+    },
+    {
+        "icon": "@",
+        "name": "Mythril Boots",
+        "primary": 16,
+        "primary_type": "Distance",
+        "secondary": 16,
+        "secondary_type": "Defence",
+        "bonus_1": "Empowered",
+        "bonus_2": "Fortified",
+        "enchant": "Fire",
+        "elite": true,
+        "price": 30,
+        "seller": "MonDaeYupe"
+    },
+    {
+        "icon": "@",
+        "name": "Emerald Boots",
+        "primary": 14,
+        "primary_type": "Mele",
+        "secondary": 7,
+        "secondary_type": "Mele",
+        "bonus_1": "HP Regen",
+        "bonus_2": "Money Find",
+        "enchant": "Ice",
+        "elite": false,
+        "price": 25,
+        "seller": "Lordwill"
+    },
+    {
+        "icon": "@",
+        "name": "Granite Boots",
+        "primary": 14,
+        "primary_type": "Mele",
+        "secondary": 14,
+        "secondary_type": "Spirit",
+        "bonus_1": "Leech",
+        "bonus_2": "Thorns",
+        "enchant": "Mana",
+        "elite": true,
+        "price": 20,
+        "seller": "COD"
+    },
+    {
+        "icon": "@",
+        "name": "Golden Boots",
+        "primary": 12,
+        "primary_type": "Mele",
+        "secondary": 6,
+        "secondary_type": "Mele",
+        "bonus_1": "Thorns",
+        "bonus_2": "Omni Resist",
+        "enchant": "Physical",
+        "elite": false,
+        "price": 15,
+        "seller": "Leia"
+    }
+]
